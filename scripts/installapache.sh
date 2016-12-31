09759ecfcc36b9d4a4f9f352a7cd61c371dffac2
Omar Habash <ohabash@successpartners.com>
2016-12-31 12:32:19 -0600

    slack notify

--- scripts/installapache.sh
+++ scripts/installapache.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 yum -y install httpd > /var/log/installapache.out 2>&1
+
+
+webhook_url="https://hooks.slack.com/services/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"
+channel="#general"
+# channel=$channel$1;
+sender="AWS-OH"
+sender=$sender": "
+$msg="Deployment detected"
+text=$sender$msg
+
+
+escapedText=$(echo $text | sed 's/"/\"/g' | sed "s/'/\'/g" )
+json="{\"channel\": \"$channel\", \"text\": \"$escapedText\"}"
+
+curl -s -d "payload=$json" "$webhook_url"
