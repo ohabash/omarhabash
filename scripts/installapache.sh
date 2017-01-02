3104113593a16e900c9171aa58781e6444fd0b9e
Omar Habash <ohabash@successpartners.com>
2017-01-02 12:59:50 -0600

    contactform configs

--- scripts/installapache.sh
+++ scripts/installapache.sh
@@ -5,7 +5,7 @@ yum -y install httpd > /var/log/installapache.out 2>&1
 webhook_url="https://hooks.slack.com/services/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"
 channel="#aws-oh"
 # channel=$channel$1;
-text="AWS-OH: Deployment detected"
+text="AWS-OH: Deployment detected $1"
 
 
 escapedText=$(echo $text | sed 's/"/\"/g' | sed "s/'/\'/g" )
